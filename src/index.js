bbac88c2a6e9bc8a805dd91d552e3d31a2420f9f
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2023-01-08 20:59:28 +0200

    v.01/08/2023

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import './css/styles.css';
+
+import Notiflix from 'notiflix'; 
+import axios, {isCancel, AxiosError} from 'axios';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const keyPixabay = '32703006-cccd8a05397b9c29232fd5c43';
+const BASE_URL = 'https://pixabay.com/api/';
+
+function fetchCountries() {
+     
+  return fetch(`https://pixabay.com/api/?key=32703006-cccd8a05397b9c29232fd5c43&q=black+cats&image_type=photo&pretty=true`).then(
+        response => {
+            if (response.status === 404) {
+                throw new Error();
+            } else return response.json();
+    }
+  ).catch(() => Notiflix.Notify.failure('Oops, there is no country with that name'));
+};
+
+fetchCountries();
