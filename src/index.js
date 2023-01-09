c71f15ee5b973953253effbc9d93ab6d7b940878
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2023-01-09 08:17:30 +0200

    v.01/09/2023

--- src/index.js
+++ src/index.js
@@ -6,17 +6,28 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 const keyPixabay = '32703006-cccd8a05397b9c29232fd5c43';
-const BASE_URL = 'https://pixabay.com/api/';
+const BASE_URL = 'https://pixabay.com/api';
 
-function fetchCountries() {
+// function getGallery() {
      
-  return fetch(`https://pixabay.com/api/?key=32703006-cccd8a05397b9c29232fd5c43&q=black+cats&image_type=photo&pretty=true`).then(
-        response => {
-            if (response.status === 404) {
-                throw new Error();
-            } else return response.json();
-    }
-  ).catch(() => Notiflix.Notify.failure('Oops, there is no country with that name'));
+//   return fetch(`https://pixabay.com/api/?key=32703006-cccd8a05397b9c29232fd5c43&q=black+cats&image_type=photo&pretty=true`).then(
+//         response => {
+//             if (response.status === 404) {
+//                 throw new Error();
+//             } else return response.json();
+//     }
+//   )
+//     .catch(() => Notiflix.Notify.failure('Oops, there is no country with that name'));
+// };
+
+
+async function getGallery() {
+  try {
+    const response = await axios.get('${BASE_URL}/api/?key=32703006-cccd8a05397b9c29232fd5c43&q=black+cats&image_type=photo&pretty=true');
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-fetchCountries();
+getGallery();
