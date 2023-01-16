f721d39e74bb39d19fbf3e45c7b9d9b992327f91
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2023-01-17 00:42:32 +0200

    v.01/16/2023-2

--- src/index.js
+++ src/index.js
@@ -5,16 +5,21 @@ import axios, {isCancel, AxiosError} from 'axios';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-
-
-const KEY_PIXABAY = '332702624-2e762d8c87a3cafce881a5f67';
-                     
+const KEY_PIXABAY = '332702624-2e762d8c87a3cafce881a5f67';            
 const BASE_URL = 'https://pixabay.com/api/';
 
+// const form = document.querySelector('#search-form');
+const input = document.querySelector('.text');
+input.addEventListener('input', debounce(getGallery, DEBOUNCE_DELAY));
+
+
+const per_page = 40;
+let page = 1;
 
 async function getGallery() {
   try {
-    const response = await axios.get(`${BASE_URL}?key=${KEY_PIXABAY}&q=black+dogs&image_type=photo&pretty=true`);
+    // const response = await axios.get(`${BASE_URL}?key=${KEY_PIXABAY}&q=black+dogs&image_type=photo&pretty=true`);
+    const response = await axios.get(`${BASE_URL}?key=${KEY_PIXABAY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${per_page}&page=${page}`);
     return response;
     
   } catch (error) {
@@ -22,4 +27,5 @@ async function getGallery() {
   }
 };
 
-getGallery();
+// getGallery();
+
