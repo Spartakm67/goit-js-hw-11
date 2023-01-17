c5dab6d9538bf1711c69f548029e3a65732390f4
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2023-01-17 17:50:09 +0200

    v.01/17/2023

--- src/index.js
+++ src/index.js
@@ -5,18 +5,29 @@ import axios, {isCancel, AxiosError} from 'axios';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+import Fetch from './js/userfetch';
+import Markup from './js/markup';
+
 const KEY_PIXABAY = '332702624-2e762d8c87a3cafce881a5f67';            
 const BASE_URL = 'https://pixabay.com/api/';
 
-// const form = document.querySelector('#search-form');
-const input = document.querySelector('.text');
-input.addEventListener('input', debounce(getGallery, DEBOUNCE_DELAY));
+const form = document.querySelector('.search-form');
 
+const divGallery = document.querySelector('.gallery');
+
+const loadMoreBtn = document.querySelector('.load-more');
+// const footer = document.querySelector('.footer');
+// const endedInfo = document.querySelector('.ended-info');
+
+form.addEventListener('submit', searchUser);
+loadMoreBtn.addEventListener('click', Markup.loadMoreBtn);
 
 const per_page = 40;
 let page = 1;
 
 async function getGallery() {
+
+  // const inputValue = input.value.trim();
   try {
     // const response = await axios.get(`${BASE_URL}?key=${KEY_PIXABAY}&q=black+dogs&image_type=photo&pretty=true`);
     const response = await axios.get(`${BASE_URL}?key=${KEY_PIXABAY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${per_page}&page=${page}`);
@@ -29,3 +40,12 @@ async function getGallery() {
 
 // getGallery();
 
+Notiflix.Notify.init({
+  position: 'center-top',
+  width: '380px',
+  distance: '30px',
+  opacity: 1,
+  borderRadius: '5px',
+  rtl: false,
+  timeout: 3000,
+});
---
+++ src/js/markup.js
@@ -0,0 +1,11 @@
+
+
+function loadMoreBtn() {
+    const inputValue = form.elements.searchQuery.value;
+    getGallery(inputValue);
+  };
+
+
+function  markupGallery() {};
+
+  export default { loadMoreBtn, markupGallery };
